eb55e927c9efaaa3d0ccdac9ac595cc2bbf4c9df
HERNANDIITO <pablohhgg@gmail.com>
2023-11-28 21:27:38 +0100

    readfile y setup de tree

--- Compendium.java
+++ Compendium.java
@@ -1,14 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Compendium {
     private ArrayList<Document> documents;
     private ArrayList<String> dictionary;
 
+    /**
+     * Iniciailza los arraylists
+     */
     public Compendium() {
         documents = new ArrayList<Document>() {};
         dictionary = new ArrayList<String>() {};
     };
 
+    public String replaceSepCharacs( String line ) {
+        String result = line;
+
+        result.replace(',', ' ');
+        result.replace(':', ' ');
+        result.replace(';', ' ');
+        result.replace('?', ' ');
+        result.replace('!', ' ');
+        result.replace('.', ' ');
+        result.replace('(', ' ');
+        result.replace(')', ' ');
+
+        return result;
+    }
+    
+    /**
+     * DICTIONARY: 
+     * word1 word2 word3
+     * doc.toString()
+     * @return result
+     */
     public String toString() {
         String result = "DICTIONARY: \n";
         for (String string : dictionary) {
@@ -23,10 +50,59 @@ public class Compendium {
         return result;
     };
 
-    public void readFile( String fileName ) {
+    public void readFile(String fileName) {
+        boolean lookingForCat = false;
+        boolean workingOnDic = false;
+        Document docAux;
+        int idCurrentDoc = 0;
 
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.equals("<DOC>")) {
+                    lookingForCat = true;
+                    idCurrentDoc++; 
+                    continue;
+                }
+
+                if ( line.equals("<DIC>") ) {
+                    workingOnDic = true;
+                    continue;
+                }
+
+                if ( lookingForCat ) {
+                    docAux = new Document( idCurrentDoc, line );
+                    this.documents.add(docAux);
+                    lookingForCat = false;
+                    continue;
+                }
+
+                line = replaceSepCharacs(line);
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if ( workingOnDic ) {
+                        System.out.println(word);
+                        this.dictionary.add(word);
+                    } else {
+                        this.documents.get(idCurrentDoc - 1).addCard(word);
+                    }
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     };
 
+
+    /**
+     * @param word
+     * @return devuelve una ArrayList de IDs cuyos documentos contienen word
+     */
     public ArrayList<Integer> search( String word ) {
         ArrayList<Integer> result = new ArrayList<Integer>() {};
 
@@ -39,10 +115,16 @@ public class Compendium {
         return result;
     };
 
+    /**
+     * @return this.documents
+     */
     public ArrayList<Document> getDocuments() {
         return documents;
     }
 
+    /**
+     * @return this.dictionary
+     */
     public ArrayList<String> getDictionary() {
         return dictionary;
     }
--- IndexTree.java
+++ IndexTree.java
@@ -1,3 +1,19 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class IndexTree {
-    
+    private TreeMap<String, TreeSet<Integer>> tree;
+
+    public IndexTree() {
+        tree = new TreeMap<String, TreeSet<Integer>>();
+    }
+
+    public boolean isEmtpy() {
+        return tree.isEmpty();
+    }
+
+    public boolean insert( String word ) {
+        boolean inserted = false;
+        return inserted;
+    }
 }
